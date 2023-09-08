0b8fc4eb170736b398c05fcbbf8df900575abde1
Yuvem <matjuve33@icloud.com>
2023-09-08 12:03:05 +0200

    Premier Test Hello World

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * Description of AccueilController
+ *
+ * @author Mateo
+ */
+class AccueilController {
+    /**
+     * @Route("/", name="accueil")
+     * @return Response
+     */
+    public function index(): Response{
+        return new Response('Hello World');
+    }
+}
