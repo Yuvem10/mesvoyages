769e005aaaab49e393ea21124f2d44372689bb90
Yuvem <matjuve33@icloud.com>
2023-09-08 12:23:20 +0200

    Premier Bloc page d'accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,19 +1,21 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 /**
  * Description of AccueilController
  *
  * @author Mateo
  */
-class AccueilController {
+class AccueilController extends AbstractController{
     /**
      * @Route("/", name="accueil")
      * @return Response
      */
     public function index(): Response{
-        return new Response('Hello World');
+        return $this->render("pages/accueil.html.twig");
     }
 }
