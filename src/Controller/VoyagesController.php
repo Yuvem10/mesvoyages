4a53b4dcb0bbe9e4aa7d6004276d100995cef07e
Yuvem <matjuve33@icloud.com>
2023-09-12 13:08:12 +0200

    Tri des enregistrements

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -20,11 +20,10 @@ class VoyagesController extends AbstractController{
      * @return Response
      */
     public function index(): Response {
-        $visites = $this->repository->findAll();
+        $visites = $this->repository->findAllOrderBy('datecreation', 'DESC');
         return $this->render("pages/voyages.html.twig", ['visites' => $visites]);
-        
-        
     }
+    
     
     /**
  * 
@@ -39,7 +38,16 @@ private $repository;
 public function __construct(VisiteRepository $repository){
     $this->repository = $repository;
 }
-
+/**
+ * @Route("/voyages/tri/{champ}/{ordre}", name="voyages.sort")
+ * @param type $champ
+ * @param type $ordre
+ * @return Response
+ */
+public function sort($champ, $ordre) : Response{
+    $visites = $this->repository->findAllOrderBy($champ, $ordre);
+    return $this->render("pages/voyages.html.twig", ['visites' => $visites]);
+}
 }
 
 
--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -45,4 +45,17 @@ class VisiteRepository extends ServiceEntityRepository
 //            ->getOneOrNullResult()
 //        ;
 //    }
+    
+    /**
+     * Retourne toutes les visites triées sur un champ
+     * @param type $champ
+     * @param type $ordre   
+     * @return Visite[]    
+     */
+    public function findAllOrderBy($champ, $ordre) : array{
+        return $this->createQueryBuilder('v')
+                ->orderBy('v.'.$champ, $ordre)
+                ->getQuery()
+                ->getResult();
+    }
 }
