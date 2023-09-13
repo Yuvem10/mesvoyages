87c717660b3c82444fb8827e510e72f82ad105bb
Yuvem <matjuve33@icloud.com>
2023-09-13 12:06:31 +0200

    Modification d'un enregistrement et création d'un formulaire

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\admin;
 
 use App\Entity\Visite;
+use App\Form\VisiteType;
 use App\Repository\VisiteRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -48,9 +49,28 @@ class AdminVoyagesController extends AbstractController {
      * @param Visite $visite
      * @return Response
      */
-    public function suppr(Visite $visite) : Response{
+    public function suppr(Visite $visite): Response {
         $this->repository->remove($visite, true);
         return $this->redirectToRoute('admin.voyages');
+    }
+
+    /**
+     * @Route("/admin/edit/{id}", name="admin.voyage.edit")
+     * @param Visite $visite
+     * @return Response
+     */
+    public function edit(Visite $visite, Request $request): Response {
+        $formVisite = $this->createForm(VisiteType::class, $visite);
+        
+        $formVisite->handleRequest($request);
+        if ($formVisite->isSubmitted()&& $formVisite->isValid()){
+            $this->repository->add($visite, true);
+            return $this->redirectToRoute('admin.voyages');
+        }
+        
+        return $this->render("admin/admin.voyage.edit.html.twig", ['visite' => $visite,
+                    'formvisite' => $formVisite->createView()
+        ]);
     }
 
 }
