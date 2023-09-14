4ff1e7defb5c836220fc9b690694e4a48f940d3e
Yuvem <matjuve33@icloud.com>
2023-09-14 11:54:38 +0200

    Gestion du lien entre les tables

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -5,42 +5,48 @@ namespace App\Form;
 use App\Entity\Visite;
 use DateTime;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class VisiteType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+class VisiteType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
-            ->add('ville')
-            ->add('pays')
-            ->add('datecreation', DateType::class, [
-                'widget' => 'single_text',
-                'data' => isset($options['data']) &&
+                ->add('ville')
+                ->add('pays')
+                ->add('datecreation', DateType::class, [
+                    'widget' => 'single_text',
+                    'data' => isset($options['data']) &&
                     $options['data']->getDateCreation() != null ? $options['data']->getDateCreation() : new DateTime('now'),
-                'label' => 'Date'
-            ])
-            ->add('note')
-            ->add('avis')
-            ->add('tempmin', null, [
-                'label' => 't° min'
-            ])
-            ->add('tempmax', null, [
-                'label' => 't° max'
-            ])
-            ->add('submit', SubmitType::class, [
-                'label' => 'Enregistrer'
-            ])
+                    'label' => 'Date'
+                ])
+                ->add('note')
+                ->add('avis')
+                ->add('tempmin', null, [
+                    'label' => 't° min'
+                ])
+                ->add('tempmax', null, [
+                    'label' => 't° max'
+                ])
+                ->add('environnements', EntityType::class, [
+                    'class' => \App\Entity\Environnement::class,
+                    'choice_label' => 'nom',
+                    'multiple' => true,
+                    'required' => false
+                ])
+                ->add('submit', SubmitType::class, [
+                    'label' => 'Enregistrer'
+                ])
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
+    public function configureOptions(OptionsResolver $resolver): void {
         $resolver->setDefaults([
             'data_class' => Visite::class,
         ]);
     }
+
 }
