78d19db85fa5d61ec131b9dc4ebdb61b0042338e
Yuvem <matjuve33@icloud.com>
2023-09-18 10:16:41 +0200

    Gestion des images

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -2,11 +2,13 @@
 
 namespace App\Form;
 
+use App\Entity\Environnement;
 use App\Entity\Visite;
 use DateTime;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -32,10 +34,15 @@ class VisiteType extends AbstractType {
                     'label' => 't° max'
                 ])
                 ->add('environnements', EntityType::class, [
-                    'class' => \App\Entity\Environnement::class,
+                    'class' => Environnement::class,
                     'choice_label' => 'nom',
                     'multiple' => true,
                     'required' => false
+                ])
+                
+                ->add('imageFile', FileType::class, [
+                    'required' => false,
+                    'label' => 'sélection image'
                 ])
                 ->add('submit', SubmitType::class, [
                     'label' => 'Enregistrer'
